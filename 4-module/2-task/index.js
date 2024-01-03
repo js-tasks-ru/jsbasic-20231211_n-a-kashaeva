93f0f9cffc56d813582e4db0d346b867cd9cc3ad
Nataliya Kashaeva <n.a.kashaeva@gmail.com>
2024-01-03 17:35:19 +0300

    Add makeDiagonalRed

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,6 @@
 function makeDiagonalRed(table) {
-  // ваш код...
+  let cells_number = table.rows.length;
+  for (let i = 0; i < cells_number; i += 1) {
+    table.rows[i].cells[i].style.backgroundColor = 'red';
+  }
 }
