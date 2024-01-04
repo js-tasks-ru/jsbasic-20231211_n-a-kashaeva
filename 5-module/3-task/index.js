7723472cbb9f8d245d25c87d615cb36162127d58
Nataliya Kashaeva <n.a.kashaeva@gmail.com>
2024-01-04 16:01:46 +0300

    initCarousel

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,32 @@
 function initCarousel() {
-  // ваш код...
+  let arrowRightButton = document.querySelector('.carousel__arrow_right');
+  let arrowLeftButton = document.querySelector('.carousel__arrow_left');
+  let carouselWindow = document.querySelector('.carousel__inner');
+  let counter = 0;
+  const totalItems = 4;
+
+  updateArrows();
+
+  function updateArrows() {
+    arrowLeftButton.style.display = counter > 0 ? '' : 'none';
+    arrowRightButton.style.display = counter < totalItems - 1 ? '' : 'none';
+  }
+
+  function carouselNext() {
+    if (counter < totalItems - 1) {
+      counter++;
+      carouselWindow.style.transform = `translateX(${-carouselWindow.offsetWidth * counter}px)`;
+    }
+    updateArrows();
+  }
+  function carouselPrevious() {
+    if (counter > 0) {
+      counter--;
+      carouselWindow.style.transform = `translateX(${-carouselWindow.offsetWidth * counter}px)`;
+    }
+    updateArrows();
+  }
+
+  arrowRightButton.addEventListener('click', carouselNext);
+  arrowLeftButton.addEventListener('click', carouselPrevious);
 }
