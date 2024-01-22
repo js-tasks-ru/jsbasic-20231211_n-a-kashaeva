0c63133e9a8320262bb04e5231ce26b6195b656a
Nataliya Kashaeva <n.a.kashaeva@gmail.com>
2024-01-22 15:13:53 +0300

    Add Cart

--- 8-module/3-task/index.js
+++ 8-module/3-task/index.js
@@ -6,23 +6,51 @@ export default class Cart {
   }
 
   addProduct(product) {
-    // ваш код
-  }
+    if (!product || product.id === undefined) {
+      return;
+    }
+    let cartItemIndex = this.cartItems.findIndex(item => item.product.id === product.id);
+
+    if (cartItemIndex === -1) {
+      const newCartItem = {
+        product: product,
+        count: 1
+      }
+      this.cartItems.push(newCartItem);
+
+      this.onProductUpdate(newCartItem);
+     } else {
+      this.updateProductCount(product.id, 1);
+    }
+    }
 
   updateProductCount(productId, amount) {
-    // ваш код
+    let cartItemIndex = this.cartItems.findIndex(item => item.product.id === productId);
+    let cartItem = this.cartItems[cartItemIndex];
+    if (cartItemIndex >= 0) {
+      cartItem.count += amount;
+      if (cartItem.count <= 0) {
+        this.cartItems.splice(cartItemIndex, 1);
+    }
+
+    this.onProductUpdate(cartItem);
   }
+}
 
   isEmpty() {
-    // ваш код
+    if (this.cartItems.length === 0) {
+      return true;
+    };
+
+    return false;
   }
 
   getTotalCount() {
-    // ваш код
+    return this.cartItems.reduce((total, item) => total + item.count, 0);
   }
 
   getTotalPrice() {
-    // ваш код
+    return this.cartItems.reduce((total, item) => total + item.count * item.product.price, 0);
   }
 
   onProductUpdate(cartItem) {
